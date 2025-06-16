d28b1689f26b58d827d55288df366d912d0ec26f
rms35 <martinsoeder35@gmail.com>
2025-06-16 12:57:02 +0200

    .h y ft

---
+++ push_swap.h
@@ -0,0 +1,10 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stddef.h>
+# include <stdlib.h>
+
+char	**ft_swap_first(char	**stack_a);
+size_t	ft_dptr_len(char **dptr);
+
+#endif
--- push_swap_utils.c
+++ push_swap_utils.c
@@ -12,43 +12,69 @@ size_t	ft_dptr_len(char **dptr)
 	return (i);
 }
 
-char	**ft_sa(char	**stack_a)
+char	**ft_swap_(char	**stack)
 {
 	int	i;
 	char	**r;
 
 	i = 2;
-	if (!stack_a)
+	if (!stack)
 		return (NULL);
-	if (ft_dptr_len(stack_a) <= 1)
+	if (ft_dptr_len(stack) <= 1)
 		return (NULL);
-	r = (char **)ft_calloc(ft_dptr_len(stack_a) + 1, sizeof(char *));
+	r = (char **)ft_calloc(ft_dptr_len(stack) + 1, sizeof(char *));
 	if (!r)
 		return (NULL);
-	r[0] = ft_strdup(stack_a[1]);
-	r[1] = ft_strdup(stack_a[0]);
-	while (stack_a[i])
+	r[0] = ft_strdup(stack[1]);
+	r[1] = ft_strdup(stack[0]);
+	while (stack[i])
 	{
-		r[i] = ft_strdup(stack_a[i]);
+		r[i] = ft_strdup(stack[i]);
 		i++;
 	}
 	r[i] = NULL;
+	free (stack);
+	stack = NULL;
 	return (r);
+}
+
+char	**ft_push(char **stack_a, char **stack_b)
+{
+	char	*t;
+	size_t	len;
+
+	if (!stack_b)
+		return (stack_a);
+	t = stack_a[0];
+	stack_a[0] = ft_strdup(stack_b[0]);
+	free(t);
+	t = NULL;
+	return (stack_a); 
 }
 
 // int	main()
 // {
-// 	char *s[4] = {"123", "-44", "0", NULL};
-// 	char **r = ft_sa(s);
+// 	char **s = (char **)calloc(4, sizeof(char *));
+// 	s[0] = ft_strdup("123");
+// 	s[1] = ft_strdup("-44");
+// 	s[2] = ft_strdup("0");
+// 	s[3] = NULL;
+// 	char **s2 = (char **)calloc(4, sizeof(char *));
+// 	s2[0] = ft_strdup("1");
+// 	s2[1] = ft_strdup("-44");
+// 	s2[2] = ft_strdup("0");
+// 	s2[3] = NULL;
+// 	s = ft_push(s, s2);
 // 	int i = 0;
 // 	while (i < 4)
 // 	{
-// 		if (r[i])
+// 		if (s[i])
 // 		{
-// 			printf("%s\n", r[i]);
-// 			free (r[i]);
+// 			printf("%s\n", s[i]);
+// 			free (s[i]);
 // 		}
 // 		i++;
 // 	}
-// 	free (r);
+// 	free (s);
+// 	ft_free_d(s2);
 // }
