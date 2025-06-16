3bd4d5638163ef7289ad2f069133de817f096768
rms35 <martinsoeder35@gmail.com>
2025-06-16 13:15:40 +0200

    ft_rev_rot

--- push_swap.h
+++ push_swap.h
@@ -7,5 +7,7 @@
 char	**ft_swap(char	**stack_a);
 size_t	ft_dptr_len(char **dptr);
 char	**ft_push(char **stack_a, char **stack_b);
+char	**ft_rev_rot(char **stack);
+char	**ft_rotate(char **stack);
 
 #endif
--- push_swap_utils.c
+++ push_swap_utils.c
@@ -52,6 +52,24 @@ char	**ft_push(char **stack_a, char **stack_b)
 	return (stack_a); 
 }
 
+char	**ft_rev_rot(char **stack)
+{
+	char	*last;
+	size_t	len;
+
+	if (!stack)
+		return (NULL);
+	len = ft_dptr_len(stack) - 1;
+	last = stack[len];
+	while (len > 0)
+	{
+		stack[len] = stack[len - 1];
+		len--;
+	}
+	stack[0] = last;
+	return (stack);
+}
+
 char	**ft_rotate(char **stack)
 {
 	char	*first;
@@ -72,30 +90,30 @@ char	**ft_rotate(char **stack)
 	return (stack);
 }
 
-// #include <stdio.h>
-// int	main()
-// {
-// 	char **s = (char **)calloc(4, sizeof(char *));
-// 	s[0] = ft_strdup("123");
-// 	s[1] = ft_strdup("-44");
-// 	s[2] = ft_strdup("0");
-// 	s[3] = NULL;
-// 	char **s2 = (char **)calloc(4, sizeof(char *));
-// 	s2[0] = ft_strdup("1");
-// 	s2[1] = ft_strdup("-44");
-// 	s2[2] = ft_strdup("0");
-// 	s2[3] = NULL;
-// 	s = ft_rotate(s);
-// 	int i = 0;
-// 	while (i < 4)
-// 	{
-// 		if (s[i])
-// 		{
-// 			printf("%s\n", s[i]);
-// 			free (s[i]);
-// 		}
-// 		i++;
-// 	}
-// 	free (s);
-// 	ft_free_d(s2);
-// }
+#include <stdio.h>
+int	main()
+{
+	char **s = (char **)calloc(4, sizeof(char *));
+	s[0] = ft_strdup("123");
+	s[1] = ft_strdup("-44");
+	s[2] = ft_strdup("0");
+	s[3] = NULL;
+	char **s2 = (char **)calloc(4, sizeof(char *));
+	s2[0] = ft_strdup("1");
+	s2[1] = ft_strdup("-44");
+	s2[2] = ft_strdup("0");
+	s2[3] = NULL;
+	s = ft_rev_rot(s);
+	int i = 0;
+	while (i < 4)
+	{
+		if (s[i])
+		{
+			printf("%s\n", s[i]);
+			free (s[i]);
+		}
+		i++;
+	}
+	free (s);
+	ft_free_d(s2);
+}
