3706cee27071d331c03b70f7659be7daa71e49a6
rms35 <martinsoeder35@gmail.com>
2025-06-23 15:20:07 +0200

    comentamos en el script para no tener output demasiado grande y ahora también imprime  combinaciones en caso de KO

--- tester.sh
+++ tester.sh
@@ -4,7 +4,7 @@
 for line in {1..2}
 do
     echo "2 números línea=$line" >> log.txt
-    value=$(./push_swap $(awk -v num="$line" 'NR==num {gsub(/"/, ""); print}' bigtests.txt) | wc -l| xargs)
+    value=$(./push_swap $(awk -v num="$line" 'NR==num {gsub(/"/, ""); print}' tests.txt) | wc -l| xargs)
     if [ "$value" -gt 1 ]; then
         echo "KO $value" >> log.txt
     else
@@ -15,7 +15,7 @@ done
 for line in {4..7}
 do
     echo "3 números línea=$line" >> log.txt
-    value=$(./push_swap $(awk -v num="$line" 'NR==num {gsub(/"/, ""); print}' bigtests.txt) | wc -l| xargs)
+    value=$(./push_swap $(awk -v num="$line" 'NR==num {gsub(/"/, ""); print}' tests.txt) | wc -l| xargs)
     if [ "$value" -gt 1 ]; then
         echo "KO $value" >> log.txt
     else
@@ -23,26 +23,27 @@ do
     fi
 done
 
-for line in {9..32}
-do
-    echo "3 números línea=$line" >> log.txt
-    value=$(./push_swap $(awk -v num="$line" 'NR==num {gsub(/"/, ""); print}' bigtests.txt) | wc -l| xargs)
-    if [ "$value" -gt 1 ]; then
-        echo "KO $value" >> log.txt
-    else
-        echo "OK $value" >> log.txt
-    fi
-done
+# for line in {9..32}
+# do
+#     echo "3 números línea=$line" >> log.txt
+#     value=$(./push_swap $(awk -v num="$line" 'NR==num {gsub(/"/, ""); print}' tests.txt) | wc -l| xargs)
+#     if [ "$value" -gt 1 ]; then
+#         echo "KO $value" >> log.txt
+#     else
+#         echo "OK $value" >> log.txt
+#     fi
+# done
 
-for line in {34..153}
-do
-    echo "5 números línea=$line"  >> log.txt
-    value=$(./push_swap $(awk -v num="$line" 'NR==num {gsub(/"/, ""); print}' bigtests.txt) | wc -l| xargs)
-    if [ "$value" -gt 1 ]; then
-        echo "KO $value" >> log.txt
-    else
-        echo "OK $value" >> log.txt
-    fi
-done
+# for line in {34..153}
+# do
+#     echo "5 números línea=$line"  >> log.txt
+#     value=$(./push_swap $(awk -v num="$line" 'NR==num {gsub(/"/, ""); print}' tests.txt) | wc -l| xargs)
+#     if [ "$value" -gt 1 ]; then
+#         echo "KO $value" >> log.txt
+#     else
+#         echo "OK $value" >> log.txt
+#     fi
+# done
 
-awk 'prev ~ /números línea/ && /KO/ {print prev} {prev=$0}' log.txt
+awk 'prev ~ /números línea/ && /KO/ {print prev "; comb = " $2} {prev=$0}' log.txt
+
